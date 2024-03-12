4f8e525e7af04408c8cda5d422a0857466b5b2cb
alllanwu <alanwu0004@gmail.com>
2024-03-12 00:45:43 -0700

    Fix nav bar radio buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import { useState } from 'react'
+// import Links from './assets/Links'
 import './App.css'
 
 function App() {
@@ -9,37 +10,52 @@ function App() {
         <h1>Resumate</h1>
       </header>
       <main>
-
+        {/* <Links /> */}
       </main>
       <nav>
-        <figure>
-          <picture>
-            <source srcSet="link-dark.png" media="(prefers-color-scheme: dark)" />
-            <img src="link-light.png" alt="link" />
-          </picture>
-          <figcaption>Links</figcaption>
-        </figure>
-        <figure>
-          <picture>
-            <source srcSet="experience-dark.png" media="(prefers-color-scheme: dark)" />
-            <img src="experience-light.png" alt="experience" />
-          </picture>
-          <figcaption>Experience</figcaption>
-        </figure>
-        <figure>
-          <picture>
-            <source srcSet="resume-dark.png" media="(prefers-color-scheme: dark)" />
-            <img src="resume-light.png" alt="resume" />
-          </picture>
-          <figcaption>Resume</figcaption>
-        </figure>
-        <figure>
-          <picture>
-            <source srcSet="profile-dark.png" media="(prefers-color-scheme: dark)" />
-            <img src="profile-light.png" alt="profile" />
-          </picture>
-          <figcaption>Profile</figcaption>
-        </figure>
+        <input type="radio" name="tab" id="links" />
+        <label htmlFor="links">
+          <figure>
+            <picture>
+              <source srcSet="links-dark.png" media="(prefers-color-scheme: dark)" />
+              <img src="links-light.png" alt="links" />
+            </picture>
+            <figcaption>Links</figcaption>
+          </figure>
+        </label>
+        <input type="radio" name="tab" id="experience" />
+
+        <label htmlFor="experience">
+          <figure>
+            <picture>
+              <source srcSet="experience-dark.png" media="(prefers-color-scheme: dark)" />
+              <img src="experience-light.png" alt="experience" />
+            </picture>
+            <figcaption>Experience</figcaption>
+          </figure>
+        </label>
+
+        <input type="radio" name="tab" id="resume" />
+        <label htmlFor="resume">
+          <figure>
+            <picture>
+              <source srcSet="resume-dark.png" media="(prefers-color-scheme: dark)" />
+              <img src="resume-light.png" alt="resume" />
+            </picture>
+            <figcaption>Resume</figcaption>
+          </figure>
+        </label>
+
+        <input type="radio" name="tab" id="profile" />
+        <label htmlFor="profile">
+          <figure>
+            <picture>
+              <source srcSet="profile-dark.png" media="(prefers-color-scheme: dark)" />
+              <img src="profile-light.png" alt="profile" />
+            </picture>
+            <figcaption>Profile</figcaption>
+          </figure>
+        </label>
       </nav>
     </>
   )
