b493444193160b60cc149979f049c13e6db0d68c
alllanwu <alanwu0004@gmail.com>
2024-04-19 20:44:08 -0700

    Restyle drag drop zone

--- src/assets/Documents.tsx
+++ src/assets/Documents.tsx
@@ -1,40 +1,44 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Document from './Document'
 import './Documents.css'
 
 function Documents() {
-  // const [prompt, setPrompt] = useState('Drag document here')
+  const [prompt, setPrompt] = useState('Drag document here')
 
-  // const setDragPrompt = () => {
-  //   setPrompt('Drag document here');
-  //   const dragDrop = document.querySelector('.drag-drop') as HTMLElement;
-  //   dragDrop.style.backgroundColor = 'var(--asset-background)';
-  // }
+  const setDragPrompt = () => {
+    setPrompt('Drag document here');
+    const dragDrop = document.querySelector('.drag-drop') as HTMLElement;
+    dragDrop.style.backgroundColor = 'var(--asset-background)';
+  }
 
-  // const setDropPrompt = () => {
-  //   setPrompt('Drop document here');
-  //   const dragDrop = document.querySelector('.drag-drop') as HTMLElement;
-  //   dragDrop.style.backgroundColor = 'var(--asset-hover)';
-  // }
+  const setDropPrompt = () => {
+    setPrompt('Drop document here');
+    const dragDrop = document.querySelector('.drag-drop') as HTMLElement;
+    dragDrop.style.backgroundColor = 'var(--asset-hover)';
+  }
 
-  // const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
-  //   e.preventDefault(); // Prevent file from being opened
-  //   setDragPrompt();
-  // }
+  const dropHandler = (e: React.DragEvent<HTMLElement>) => {
+    e.preventDefault(); // Prevent file from being opened
+    setDragPrompt();
+  }
 
-  // const dragOverHandler = (e: React.DragEvent<HTMLDivElement>) => {
-  //   e.preventDefault(); // Prevent file from being opened
-  //   setDropPrompt();
-  // }
+  const dragOverHandler = (e: React.DragEvent<HTMLElement>) => {
+    e.preventDefault(); // Prevent file from being opened
+    setDropPrompt();
+  }
 
   return (
-    <div className='documents asset-container'>
-      <Document title={'Resume'} />
-      {/* <div className='drag-drop' onDragOver={dragOverHandler} onDragLeave={setDragPrompt} onDragEnd={setDragPrompt} onDrop={dropHandler}>
-        <p>{prompt}</p>
-        <input type="file" />
-      </div> */}
-    </div>
+    <>
+      <div className='assets documents'>
+        <Document title={'Resume'} file={"test.pdf"} />
+        <div className='drag-drop' onDragOver={dragOverHandler} onDragLeave={setDragPrompt} onDragEnd={setDragPrompt} onDrop={dropHandler}>
+          <p>{prompt}</p>
+          <input type="file" />
+        </div>
+      </div>
+      <br />
+      <button className='asset-add-button'>+</button>
+    </>
   )
 }
 
