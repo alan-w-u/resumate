b1dee3d56acb3001eac714b1d9fd52ed594dd743
alllanwu <alanwu0004@gmail.com>
2024-04-19 20:43:49 -0700

    Add file preview window

--- src/assets/Document.tsx
+++ src/assets/Document.tsx
@@ -1,11 +1,21 @@
 import './Document.css'
 
-function Document(props: { title: string }) {
+function Document(props: { title: string; file: string }) {
+  const copy = (e: React.DragEvent<HTMLElement>) => {
+    // e.dataTransfer.setData("application/pdf", props.file)
+    const file = new File([""], props.file, { type: "application/pdf" });
+    e.dataTransfer.setData("application/pdf", file.name)
+  }
+
   return (
-    <div className='document asset'>
-      <span className='title'>{props.title}</span>
-      <iframe src="test.pdf" ></iframe>
-    </div>
+    <a href="test.pdf" className='asset document' onDragStart={copy} draggable download>
+      <div className='document-info'>
+        <span className='document-title'>{props.title}</span>
+        <br />
+        <span className='document-file'>{props.file}</span>
+      </div>
+      <iframe src={props.file} ></iframe>
+    </a>
   )
 }
 
