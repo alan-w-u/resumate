568cb47601e83421cffb39e15a1ef0aaa1a70be5
alllanwu <alanwu0004@gmail.com>
2024-05-04 00:58:34 -0700

    Improve paste functionality and usage with emails

--- src/assets/Link.tsx
+++ src/assets/Link.tsx
@@ -5,17 +5,23 @@ function Link(props: { title: string; url: string }) {
     navigator.clipboard.writeText(props.url);
   }
 
+  const paste = (e: React.DragEvent<HTMLElement>) => {
+    // Set MIME types to paste only the URL
+    e.dataTransfer.setData('text/uri-list', props.url);
+    e.dataTransfer.setData('text/html', props.url);
+  }
+
   return (
-    <a href={props.url} className='asset link' onClick={copy} draggable>
+    <div className='asset link' onClick={copy} onDragStart={paste} draggable>
       <span className='link-title'>{props.title}</span>
       <span className='link-url'>{props.url}</span>
       <a href={props.url} target='_blank'>
         <picture>
           <source srcSet="redirect-dark.png" media="(prefers-color-scheme: dark)" />
-          <img src="redirect-light.png" alt="profile" draggable="false" />
+          <img src="redirect-light.png" alt="redirect" draggable="false" />
         </picture>
       </a>
-    </a>
+    </div>
   )
 }
 
--- src/assets/Links.tsx
+++ src/assets/Links.tsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react'
 import Link from './Link'
 import './Links.css'
 
@@ -6,6 +5,7 @@ function Links() {
   return (
     <>
       <div className='assets links'>
+        <Link title={'Email'} url={'alanwu0004@gmail.com'} />
         <Link title={'LinkedIn'} url={'https://www.linkedin.com/in/alan-w-u/'} />
         <Link title={'GitHub'} url={'https://github.com/alan-w-u'} />
         <Link title={'Website'} url={'https://alan-w-u.github.io/'} />
