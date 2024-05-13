e3fb73a3154dc14c26a6ccf4b52d89f88ecc133f
alllanwu <alanwu0004@gmail.com>
2024-05-12 17:39:35 -0700

    Began implementation for drag and drop files

--- src/assets/Document.tsx
+++ src/assets/Document.tsx
@@ -1,22 +1,75 @@
+import { useEffect } from 'react';
 import './Document.css'
 
 function Document(props: { title: string; file: string }) {
-  const copy = (e: React.DragEvent<HTMLElement>) => {
-    // e.dataTransfer.setData("application/pdf", props.file)
-    const file = new File([""], props.file, { type: "application/pdf" });
-    e.dataTransfer.setData("application/pdf", file.name)
-  }
+  const windowURL = window.location.href.replace('index.html', ''); // Output Example: chrome-extension://poibaddlheccibebnomlggmmmojidpje/
+
+  let response: any;
+  let blob: any;
+  let objectURL: any;
+  let pdfData: any;
+
+  useEffect(() => {
+    const fetchFile = async () => {
+      response = await fetch(props.file);
+      blob = await response.blob();
+      objectURL = URL.createObjectURL(blob);
+
+      let reader = new FileReader();
+      reader.readAsArrayBuffer(blob);
+
+      reader.onload = () => {
+        pdfData = reader.result;
+      }
+    };
+
+    fetchFile();
+  }, [props.file]);
+
+  const upload = (e: React.DragEvent<HTMLElement>) => {
+    // let anchor = document.createElement('a');
+    // anchor.href = objectURL;
+    // anchor.download = props.file;
+    // anchor.click();
+
+    e.dataTransfer.setData('text/uri-list', windowURL + props.file);
+    e.dataTransfer.setData('text/html', pdfData);
+    e.dataTransfer.setData('application/pdf', pdfData);
+    // e.dataTransfer.setData('DownloadURL', `application/pdf:${props.file}:chrome-extension://poibaddlheccibebnomlggmmmojidpje/test.pdf`);
+    // e.dataTransfer.setData('DownloadURL', `application/pdf:${props.file}:${objectURL}`); // Download file when drag to desktop
+    e.dataTransfer.setData('DownloadURL', `application/pdf:${props.file}:${windowURL}${props.file}`); // Download file when drag to desktop
+
+    console.log(windowURL);
+    console.log(response);
+    console.log(response.url);
+    console.log(objectURL);
+    console.log(blob);
+    console.log(pdfData);
+  };
 
   return (
-    <a href="test.pdf" className='asset document' onDragStart={copy} draggable download>
+    <div className='asset document' onDragStart={upload} draggable>
       <div className='document-info'>
         <span className='document-title'>{props.title}</span>
-        <br />
         <span className='document-file'>{props.file}</span>
+        <div className='document-buttons'>
+          <a href={props.file} download>
+            <picture>
+              <source srcSet="download-dark.png" media="(prefers-color-scheme: dark)" />
+              <img src="download-light.png" alt="download" />
+            </picture>
+          </a>
+          <a href={props.file} target='_blank'>
+            <picture>
+              <source srcSet="redirect-dark.png" media="(prefers-color-scheme: dark)" />
+              <img src="redirect-light.png" alt="redirect" />
+            </picture>
+          </a>
+        </div>
       </div>
       <iframe src={props.file} ></iframe>
-    </a>
-  )
+    </div>
+  );
 }
 
 export default Document
